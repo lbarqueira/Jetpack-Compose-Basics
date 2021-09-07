9d0d66532d84298865ca11c2eb235a0020a427f7
Luis Barqueira <65919567+lbarqueira@users.noreply.github.com>
2021-09-07 20:06:22 +0100

    commit 7th September - state and flexible layouts

--- app/src/main/java/com/lbarqueira/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/lbarqueira/jetpackcomposebasics/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.lbarqueira.jetpackcomposebasics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices.NEXUS_6
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lbarqueira.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
@@ -43,14 +50,36 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
-    Column {
-        for (name in names) {
+fun MyScreenContent(
+    names: List<String> = List(1000) { "Hello Android $it" }
+) {
+    // state hoisting
+    val counterState: MutableState<Int> = remember { mutableStateOf(0) }
+
+    Column(modifier = Modifier.fillMaxHeight()) {
+        NameList(
+            names = names,
+            modifier = Modifier
+                .weight(1f)
+                .background(color = Color.Cyan)
+        )
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount -> counterState.value = newCount }
+        )
+    }
+}
+
+@Composable
+fun NameList(names: List<String>, modifier: Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(items = names) { name ->
             Greeting(name = name)
             Divider(color = Color.Black)
         }
     }
 }
+
 
 // Note: Composable functions are Kotlin functions that are marked with the @Composable annotation,
 // as you can see:
@@ -64,8 +93,34 @@ fun Greeting(name: String) {
 
 }
 
+// Since consumers of Counter can be interested in the state, you can defer it
+// to the caller completely by introducing a (count, updateCount) pair as parameters of Counter.
+// In this way, Counter is hoisting its state.
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    // val count: MutableState<Int> = remember { mutableStateOf(0) }
+    Button(
+        onClick = { updateCount(count + 1) },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = if (count > 5) Color.Green else Color.White
+        )
+    ) {
+        Text("I`ve been clicked $count times")
+    }
+}
 
-@Preview(name = "MyScreen preview")
+@Preview(
+    name = "MyScreen preview night mode",
+    showSystemUi = true,
+    device = NEXUS_6,
+    uiMode = UI_MODE_NIGHT_YES
+)
+@Preview(
+    name = "MyScreen preview day mode",
+    showSystemUi = true,
+    device = NEXUS_6,
+    uiMode = UI_MODE_NIGHT_NO
+)
 @Composable
 fun DefaultPreview() {
     MyApp {
